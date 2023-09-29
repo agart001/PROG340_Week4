1477082d23d7f0195226450ac4f8f315ab3218f4
agart001 <agart.art@gmail.com>
2023-09-28 20:46:45 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
-import * as THREE from "./three.module.js";
+import * as THREE from 'three';
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
-THREE.updateProjectionMatrix();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 2;
+camera.UpdateProjectionMatrix();
 
-scene.background = new THREE.Color( 0x2980b9 );
-
-var light = new THREE.PointLight( 0xffffff, 1, 100 );
-light.position.set( 10, 10, 10 );
-scene.add( light );
-
-var renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-window.addEventListener('resize', () => {
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    camera.aspect = window.innerWidth / window.innerHeight;
-    THREE.updateProjectionMatrix();
-});
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshLambertMaterial( { color: 0xFFCC00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-var box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({ color: 0xff0000 }));
-scene.add(box);
+const light = new THREE.PointLight( 0xff0000, 1, 100 );
+light.position.set( 10, 0, 25 );
+scene.add( light );
 
-renderer.render( scene, camera );
+
+renderer.render( scene, camera )
